686a687b6dda738a53ff4f4c6628e55f5431803a
The Machine <26176863+woodedjurassicpark@users.noreply.github.com>
2022-11-07 20:27:04 -0800

    Create run.py

---
+++ week2/run.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+import os
+import requests
+
+directory = "/data/feedback"
+feedback_dict = {"title":"","name":"","date":"","feedback":""};
+
+for filename in os.listdir(directory):
+    file = os.path.join(directory, filename)
+    #print(file)
+    with open(file, 'r') as f:
+        for key in feedback_dict.keys():
+            #print(key)
+            feedback_dict[key] = f.readline().strip()
+    response = requests.post("http://34.27.136.45/feedback/", json=feedback_dict)
+    print(response.request.url)
+    print(response.status_code)
+    print(response.request.body)
+    #print(feedback_dict)
